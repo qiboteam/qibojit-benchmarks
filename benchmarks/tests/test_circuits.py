ef8a32177afe8e6d8a027a09f73c9e93c26c0d9f
Stavros Efthymiou <35475381+stavros11@users.noreply.github.com>
2021-07-18 14:12:58 +0400

    Add qft circuit depth test

--- benchmarks/tests/test_circuits.py
+++ benchmarks/tests/test_circuits.py
@@ -3,6 +3,20 @@ from benchmarks import circuits
 from qibo.models import Circuit
 
 
+@pytest.mark.parametrize("swaps", ["True", "False"])
+def test_qft_circuit(swaps):
+    circuit = Circuit(28)
+    gates = circuits.QFT(28, swaps=swaps)
+    circuit.add(gates)
+    assert circuit.nqubits == 28
+    if swaps == "True":
+        assert circuit.depth == 56
+        assert circuit.ngates == 420
+    else:
+        assert circuit.depth == 55
+        assert circuit.ngates == 406
+
+
 def test_bernstein_vazirani_circuit():
     circuit = Circuit(28)
     gates = circuits.BernsteinVazirani(28)
