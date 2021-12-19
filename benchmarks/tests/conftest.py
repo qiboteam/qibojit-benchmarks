687d0d0c1d6491c7faf1051206b41d763e7ac0a0
Marco Lazzarin <48728634+mlazzarin@users.noreply.github.com>
2021-12-19 12:10:13 +0400

    Fix CI

--- benchmarks/tests/conftest.py
+++ benchmarks/tests/conftest.py
@@ -1,6 +1,7 @@
 NQUBITS = [3, 4, 5]
 BACKENDS = ["qibojit", "tensorflow", "numpy"]
-LIBRARIES = ["qibo", "qiskit", "qiskit-default", "cirq", "qulacs"]
+LIBRARIES = ["qibo", "qiskit", "qiskit-default", "qiskit-twoqubitfusion",
+             "cirq", "qsim", "tfq", "qulacs"]
 LIBRARIES_GPU = ["qiskit-gpu", "qulacs-gpu", "qcgpu"]
 
 
