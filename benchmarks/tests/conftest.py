a77a86ebb974f27ee941f15dbf0af83d5934dc54
Marco Lazzarin <48728634+mlazzarin@users.noreply.github.com>
2021-12-20 11:50:25 +0400

    Add options for GPU tests

--- benchmarks/tests/conftest.py
+++ benchmarks/tests/conftest.py
@@ -1,20 +1,30 @@
 NQUBITS = [3, 4, 5]
 BACKENDS = ["qibojit", "tensorflow", "numpy"]
 LIBRARIES = ["qibo", "qiskit", "qiskit-default", "cirq", "qulacs"]
-LIBRARIES_GPU = ["qiskit-gpu", "qulacs-gpu", "qcgpu"]
 
 
-# Check if GPU is available for tests
-try:
-    from cupy import cuda # pylint: disable=E0401
-    gpu_available = cuda.runtime.getDeviceCount()
-except:
-    gpu_available = 0
-if gpu_available:
-    LIBRARIES.extend(LIBRARIES_GPU)
+def pytest_addoption(parser):
+    parser.addoption("--qiskit_gpu", action="store_true", default=False)
+    parser.addoption("--qulacs_gpu", action="store_true", default=False)
+    parser.addoption("--qcgpu", action="store_true", default=False)
 
 
 def pytest_generate_tests(metafunc):
+
+    # Check if GPU is available for tests
+    try:
+        from cupy import cuda # pylint: disable=E0401
+        gpu_available = cuda.runtime.getDeviceCount()
+    except:
+        gpu_available = 0
+    if gpu_available:
+        if metafunc.config.option.qiskit_gpu:
+            LIBRARIES.append("qiskit-gpu")
+        if metafunc.config.option.qulacs_gpu:
+            LIBRARIES.append("qulacs-gpu")
+        if metafunc.config.option.qcgpu:
+            LIBRARIES.append("qcgpu")
+
     if "nqubits" in metafunc.fixturenames:
         metafunc.parametrize("nqubits", NQUBITS)
     if "backend" in metafunc.fixturenames:
