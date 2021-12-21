09b1ea271096c0b11a9c6d1f60118b8309f617dc
Andrea Pasquale <andreapasquale97@gmail.com>
2021-12-21 11:30:03 +0100

    Implement Marco's fixes
    Co-authored-by: Marco Lazzarin <48728634+mlazzarin@users.noreply.github.com>

--- benchmarks/tests/test_libraries.py
+++ benchmarks/tests/test_libraries.py
@@ -92,9 +92,7 @@ def test_two_qubit_gate_parametrized(nqubits, library, gate, qibo_gate, params):
     if gate in {"cu1", "cu2", "cu3"} and library == "tfq":
         pytest.skip("Skipping {} test because it is not supported by {}."
                     "".format(gate, library))
-    if gate == "cu3" and library in ["qiskit", "qiskit-default",
-                                    "qiskit-twoqubitfusion",
-                                    "qiskit-gpu"]:
+    if gate == "cu3" and "qiskit" in library:
         gate = "cu"
         params["gamma"] = - (params["phi"] + params["lam"])/2
 
@@ -102,7 +100,7 @@ def test_two_qubit_gate_parametrized(nqubits, library, gate, qibo_gate, params):
     angles = ",".join(str(params.get(n)) for n in order if n in params)
     qasm_circuit = qasm.TwoQubitGate(nqubits, gate=gate, angles=angles)
     if "gamma" in params:
-        del params["gamma"]
+        params.remove("gamma")
     target_circuit = qibo.TwoQubitGate(nqubits, gate=qibo_gate, **params)
     backend = libraries.get(library)
     assert_circuit_execution(backend, qasm_circuit, target_circuit)
