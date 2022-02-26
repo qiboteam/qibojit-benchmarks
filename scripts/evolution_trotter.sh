19e3d2810fd964fb9baaad154f2537fd5ca388ad
stavros11 <35475381+stavros11@users.noreply.github.com>
2022-02-26 16:47:22 +0400

    Update evolution bash scripts

--- scripts/evolution_trotter.sh
+++ scripts/evolution_trotter.sh
@@ -1,10 +1,12 @@
 #! /usr/bin/bash
+# Generates data for the evolution scaling with dt using the Trotter decomposition
 
 # Command-line parameters
 : "${nqubits:=10}"
 : "${precision:=double}"
-: "${filename:=qibo_evolution_trotter.dat}"
+: "${filename:=evolution.dat}"
 : "${nreps:=10}"
+
 
 # GPU qibojit backend
 for dt in 0.1 0.095 0.09 0.085 0.08 0.075 0.07 0.065 0.06 0.055 0.05 0.045 \
@@ -18,7 +20,7 @@ do
   done
 done
 
-# GPU TensorFlow and QiboTF backends
+# GPU tensorflow and qibotf backends
 for dt in 0.1 0.095 0.09 0.085 0.08 0.075 0.07 0.065 0.06 0.055 0.05 0.045 \
           0.04 0.035 0.03 0.025 0.02 0.015 0.01 0.005
 do
