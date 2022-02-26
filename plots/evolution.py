354cc1772f558a1ca168f9b33e641c3b2715616e
stavros11 <35475381+stavros11@users.noreply.github.com>
2022-02-26 15:41:58 +0400

    Add evolution plots

---
+++ plots/evolution.py
@@ -0,0 +1,99 @@
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+import seaborn as sns
+from matplotlib.patches import Patch
+matplotlib.rcParams['mathtext.fontset'] = 'cm'
+matplotlib.rcParams['font.family'] = 'STIXGeneral'
+
+
+def plot_dense(data, quantity, nqubits, fontsize=30, legend=True, save=False):
+    matplotlib.rcParams["font.size"] = fontsize
+
+    cpu_cp = sns.color_palette("Oranges", 4)
+    gpu_cp = sns.color_palette("Purples", 4)
+
+    data["is_gpu"] = data["device"].apply(lambda x: "GPU" in x)
+    base_condition = (data["nqubits"] == nqubits) & (data["dense"] == True)
+
+    plt.figure(figsize=(16, 9))    
+    condition = base_condition & (data["backend"] == "numpy")
+    plt.semilogy(data[condition]["dt"], data[condition][quantity], marker="^", markersize=10,
+                 color=cpu_cp[3], linewidth=3.0, label="numpy")
+    condition = base_condition & (data["backend"] == "tensorflow") & (data["is_gpu"] == False)
+    plt.semilogy(data[condition]["dt"], data[condition][quantity], marker="o", markersize=10,
+                 color=cpu_cp[1], linewidth=3.0, label="tensorflow cpu")
+
+    condition = base_condition & (data["backend"] == "qibojit") & (data["platform"] == "cuquantum")
+    plt.semilogy(data[condition]["dt"], data[condition][quantity], marker="^", markersize=10,
+                     color=gpu_cp[3], linewidth=3.0, label="cupy")
+    condition = base_condition & (data["backend"] == "tensorflow") & (data["is_gpu"] == True)
+    plt.semilogy(data[condition]["dt"], data[condition][quantity], marker="o", markersize=10,
+                     color=gpu_cp[1], linewidth=3.0, label="tensorflow gpu")
+
+    plt.title(f"Dense adiabatic evolution, {nqubits} qubits, double precision")
+    plt.xlabel("$\delta t$")
+    if quantity == "total_dry_time":
+        plt.ylabel("Total dry run time (sec)")
+    elif quantity == "total_simulation_time":
+        plt.ylabel("Total simulation time (sec)")
+
+    if legend:
+        plt.legend()
+
+    if save:
+        plt.savefig(filename, bbox_inches="tight")
+    else:
+        plt.show()
+
+
+def plot_trotter(data, quantity, nqubits, fontsize=30, legend=False, save=False):
+    matplotlib.rcParams["font.size"] = fontsize
+    
+    cpu_cp = sns.color_palette("Oranges", 4)
+    gpu_cp = sns.color_palette("Purples", 4)
+    
+    data["is_gpu"] = data["device"].apply(lambda x: "GPU" in x)
+    base_condition = (data["nqubits"] == nqubits) & (data["dense"] == False)
+
+    plt.figure(figsize=(16, 9))
+    condition = base_condition & (data["backend"] == "numpy")
+    plt.semilogy(data[condition]["dt"], data[condition][quantity], marker="s", markersize=10,
+                     color=cpu_cp[0], linewidth=3.0, label="numpy")
+    condition = base_condition & (data["backend"] == "tensorflow") & (data["is_gpu"] == False)
+    plt.semilogy(data[condition]["dt"], data[condition][quantity], marker="o", markersize=10,
+                 color=cpu_cp[1], linewidth=3.0, label="tensorflow cpu")
+    condition = base_condition & (data["backend"] == "qibotf") & (data["is_gpu"] == False)
+    plt.semilogy(data[condition]["dt"], data[condition][quantity], marker="D", markersize=10,
+                     color=cpu_cp[2], linewidth=3.0, label="qibotf cpu")
+    condition = base_condition & (data["backend"] == "qibojit") & (data["platform"] == "numba")
+    plt.semilogy(data[condition]["dt"], data[condition][quantity], marker="^", markersize=10,
+                     color=cpu_cp[3], linewidth=3.0, label="qibojit (numba) cpu")
+
+    condition = base_condition & (data["backend"] == "tensorflow") & (data["is_gpu"] == True)
+    plt.semilogy(data[condition]["dt"], data[condition][quantity], marker="o", markersize=10,
+                     color=gpu_cp[1], linewidth=3.0, label="tensorflow gpu")
+    condition = base_condition & (data["backend"] == "qibotf") & (data["is_gpu"] == True)
+    plt.semilogy(data[condition]["dt"], data[condition][quantity], marker="D", markersize=10,
+                     color=gpu_cp[2], linewidth=3.0, label="qibotf gpu")
+    condition = base_condition & (data["backend"] == "qibojit") & (data["platform"] == "cupy")
+    plt.semilogy(data[condition]["dt"], data[condition][quantity], marker="^", markersize=10,
+                     color=gpu_cp[3], linewidth=3.0, label="qibojit (cupy) gpu")
+    condition = base_condition & (data["backend"] == "qibojit") & (data["platform"] == "cuquantum")
+    plt.semilogy(data[condition]["dt"], data[condition][quantity], marker="v", markersize=10, linestyle="--",
+                     color=gpu_cp[3], linewidth=3.0, label="qibojit (cuquantum) gpu")
+
+    plt.title(f"Trotter adiabatic evolution, {nqubits} qubits, double precision")
+    plt.xlabel("$\delta t$")
+    if quantity == "total_dry_time":
+        plt.ylabel("Total dry run time (sec)")
+    elif quantity == "total_simulation_time":
+        plt.ylabel("Total simulation time (sec)")
+
+    if legend:
+        plt.legend(fontsize="small")
+    
+    if save:
+        plt.savefig(filename, bbox_inches="tight")
+    else:
+        plt.show()
--- plots/utils.py
+++ plots/utils.py
@@ -22,3 +22,15 @@ def load_data_multigpu(filename, qibojit_only=False):
     data["accelerators"] = data["library_options"].apply(lambda x: x.split(",")[1].split("=")[-1])
     data["nqubits (accelerators)"] = data.apply(lambda x: f"{x.nqubits} ({x.accelerators})", axis=1)
     return data
+
+
+def load_evolution_data(filename):
+    with open(filename, "r") as file:
+        data = pd.DataFrame(json.load(file))
+    
+    data["creation_time"] = data["hamiltonian_creation_time"] + data["evolution_creation_time"]
+    data["total_dry_time"] = data["dry_run_time"] + data["creation_time"] + data["import_time"]
+    #if "simulation_times_mean" not in data.columns:
+    #    data["simulation_times_mean"] = data["simulation_times"].apply(lambda x: np.mean(x))
+    data["total_simulation_time"] = data["simulation_times_mean"] + data["creation_time"] + data["import_time"]
+    return data
