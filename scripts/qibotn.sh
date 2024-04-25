5dd4cd32c2c1cc2a7ed76da8703552011973d105
tankya2 <tankya2@ihpc.a-star.edu.sg>
2024-04-25 12:15:47 +0800

    Comment out the generation of json files in script though user who prefer to keep everything in 1 script can still use it by uncommenting it

--- scripts/qibotn.sh
+++ scripts/qibotn.sh
@@ -6,21 +6,21 @@
 : "${nreps:=3}"
 : "${filename:=qibotn_expectation_double_2x4.dat}"
 
-node_list=/nodelist_2x4 #file containing the IP address of the resources
+node_list=/nodelist_2x4 #file containing the IP address of the resources, omit this if it is running on single-node-multi-gpu
 
 for (nqubits = 10; nqubits <= 8000; nqubits += 100)
 do
-    echo '{"MPI_enabled": true, "MPS_enabled": false, "NCCL_enabled": false, "expectation_enabled": true}' > cu_tensornet_mpi_expectation.json
+    #echo '{"MPI_enabled": true, "MPS_enabled": false, "NCCL_enabled": false, "expectation_enabled": true}' > cu_tensornet_mpi_expectation.json #can be pre-generated or can include it in script. Here is using pre-generated.
     mpirun -np 8 -hostfile $node_list python compare.py --circuit variational --circuit-options nlayers=3 --nqubits 4$nqubits --filename $filename \
                                             --library-options backend=qibotn,platform=cutensornet,computation_settings=cu_tensornet_mpi_expectation.json \
                                             --nreps $nreps --precision $precision
 echo
-    echo '{"MPI_enabled": false, "MPS_enabled": false, "NCCL_enabled": true, "expectation_enabled": true}' > cu_tensornet_nccl_expectation.json
+    #echo '{"MPI_enabled": false, "MPS_enabled": false, "NCCL_enabled": true, "expectation_enabled": true}' > cu_tensornet_nccl_expectation.json
     mpirun -np 8 -hostfile $node_list python compare.py --circuit variational --circuit-options nlayers=3 --nqubits $nqubits --filename $filename \
                                             --library-options backend=qibotn,platform=cutensornet,computation_settings=cu_tensornet_nccl_expectation.json \
                                             --nreps $nreps --precision $precision
 echo
-    echo '{"MPI_enabled": false, "MPS_enabled": false, "NCCL_enabled": false, "expectation_enabled": true}' > cu_tensornet_expectation.json
+    #echo '{"MPI_enabled": false, "MPS_enabled": false, "NCCL_enabled": false, "expectation_enabled": true}' > cu_tensornet_expectation.json
     python compare.py --circuit variational --circuit-options nlayers=3 --nqubits $nqubits --filename $filename \
                                             --library-options backend=qibotn,platform=cutensornet,computation_settings=cu_tensornet_expectation.json \
                                             --nreps $nreps --precision $precision
